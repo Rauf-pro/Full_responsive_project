efb03343c21388e647199d53866d2fd15e26b540
rrauf8178 <rrauf8178@gmail.com>
2022-12-26 21:07:53 +0400

    Created Navbar component wit responsive style and functionality

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
