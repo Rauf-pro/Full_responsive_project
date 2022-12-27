ad010c6c71cb69bb935f8cfc01065324914ad6e4
rrauf8178 <rrauf8178@gmail.com>
2022-12-27 12:19:20 +0400

    Added Herosection, CarItem and Cards components

---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destination</h1>
+    </div>
+  )
+}
+
+export default Cards
